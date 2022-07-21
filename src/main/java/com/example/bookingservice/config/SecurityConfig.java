df34f4100887091f750a0b6730cfcd14d0f21b24
Alexey <superpupergametsar@gmail.com>
2022-07-21 12:39:00 +0300

    added custom login form

--- src/main/java/com/example/bookingservice/config/SecurityConfig.java
+++ src/main/java/com/example/bookingservice/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import com.example.bookingservice.services.GuestService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
@@ -19,6 +20,22 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         this.guestService = guestService;
     }
 
+    @Override
+    protected void configure(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity
+                .csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/auth/login", "/error").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .loginPage("/auth/login")
+                .loginProcessingUrl("/process_login")
+                .defaultSuccessUrl("/room", true)
+                .failureUrl("/auth/login?error");
+    }
+
+    @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(guestService);
     }
